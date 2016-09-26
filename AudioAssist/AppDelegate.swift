5c02d621e0f782f52c85d306fbc7b93c7b6e5650
wiseguy16 <gergusa04@gmail.com>
2016-09-26 19:15:03 -0400

    initial commit

--- AudioAssist/AppDelegate.swift
+++ AudioAssist/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        FIRApp.configure()
+
         // Override point for customization after application launch.
         return true
     }
